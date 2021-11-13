f37ed34f14fdacf4610b09b5b1e2d9d036a2768c
victor-paravatti <guibis17@gmail.com>
2021-11-13 19:15:03 -0300

    Alteração do icone Carrosel

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -17,32 +17,39 @@ class Header extends Component {
                 <div className="slider">
                     <ul className="slides">
                         <li>
-                            <img src="https://lorempixel.com/580/250/nature/1"/> 
-                            <div className ="caption center-align">
-                            <h3>É hora de brilahar!</h3>
-                            <h5 className ="light grey-text text-lighten-3">O trabalho em equipe engrandece as pessoas</h5>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="https://lorempixel.com/580/250/nature/2"/> 
+                            <img className="responsive-img" className="" src="https://ik.imagekit.io/VictorParavatti/Aula_desenvolvimento_de_redes/cherrydeck-rMILC1PIwM0-unsplash_UHPps12C5.jpg?updatedAt=1636833008147"/> 
                             <div className ="caption left-align">
-                            <h3>Temos integração com GitHub para vc ficar desperocupado</h3>
-                            <h5 className ="light grey-text text-lighten-3">Indo sempre na vanguarda da tecnologia</h5>
+                            <h3 className ="black-text">É hora de brilhar!</h3>
+                            <h5 className ="black-text">O trabalho em equipe engrandece as pessoas</h5>
                             </div>
                         </li>
                         <li>
-                            <img src="https://lorempixel.com/580/250/nature/3"/> 
+                            <img className="responsive-img" src="https://ik.imagekit.io/VictorParavatti/Aula_desenvolvimento_de_redes/chang-duong-Sj0iMtq_Z4w-unsplash__q8rle-nurT.jpg?updatedAt=1636833009562"/> 
+                            <div className ="caption left-align">
+                            <h3 className ="black-text">Temos integração com GitHub para vc ficar desperocupado</h3>
+                            <h5 className ="black-text">Indo sempre na vanguarda da tecnologia</h5>
+                            </div>
+                        </li>
+                        <li>
+                            <img className="responsive-img" src="https://ik.imagekit.io/VictorParavatti/Aula_desenvolvimento_de_redes/kristin-wilson-z3htkdHUh5w-unsplash_bcpAdFeMQ.jpg?updatedAt=1636833009666"/> 
                             <div className ="caption right-align">
-                            <h3>Nunca para de sonhar com uma equipe unida </h3>
-                            <h5 className ="light grey-text text-lighten-3">Pense grande 
+                            <h3 className ="black-text">Nunca para de sonhar com uma equipe unida </h3>
+                            <h5 className ="black-text">Pense grande 
                             </h5>
                             </div>
                         </li>
                         <li>
-                            <img src="https://lorempixel.com/580/250/nature/4"/> 
+                            <img className="responsive-img" src="https://ik.imagekit.io/VictorParavatti/Aula_desenvolvimento_de_redes/clay-banks-LjqARJaJotc-unsplash_6u7rbh9Kr.jpg?updatedAt=1636833010915"/> 
                             <div className ="caption center-align">
-                            <h3>Siga o seu caminho </h3>
-                            <h5 className ="light grey-text text-lighten-3"></h5>
+                            <h3 className ="black-text">Siga o seu caminho </h3>
+                            <h5 className ="black-text"></h5>
+                            </div>
+                        </li>
+                        <li>
+                            <img className="responsive-img" src="https://ik.imagekit.io/VictorParavatti/Aula_desenvolvimento_de_redes/surface-AKOlQ6dE8Nc-unsplash_3__ktTDydVxz.jpg?updatedAt=1636836767726"/> 
+                            <div className ="caption center-align">
+                            <h3 className ="black-text">Siga o seu caminho </h3>
+                            <h5 className ="black-text"></h5>
                             </div>
                         </li>
                     </ul>
